bfe50df0a6331fbb84c7fa5a38c12cb9e650848c
Colm Laro <smartman97@gmail.com>
2016-01-06 09:15:22 -0700

    Home Screen can change to all others

--- AboutMeApp/ViewController.swift
+++ AboutMeApp/ViewController.swift
@@ -20,6 +20,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func FoodButton(sender: UIButton)
+    {
+        performSegueWithIdentifier("toFood", sender: sender)
+    }
+    
+    @IBAction func FamilyButton(sender: UIButton)
+    {
+        performSegueWithIdentifier("toFamily", sender: sender)
+    }
+    
+    @IBAction func MusicButton(sender: UIButton)
+    {
+        performSegueWithIdentifier("toMusic", sender: sender)
+    }
+    
+    @IBAction func SportsButton(sender: UIButton)
+    {
+        performSegueWithIdentifier("toSports", sender: sender)
+    }
+    
+    
+    @IBAction func TechButton(sender: UIButton)
+    {
+        performSegueWithIdentifier("toTechnology", sender: sender)
+    }
 }
 
